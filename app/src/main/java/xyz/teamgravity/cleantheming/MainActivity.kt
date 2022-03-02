dbcfd80f9f162b2873a25df7edb37360a79f0ec1
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-02 11:28:13 +0500

    applying spacing

--- app/src/main/java/xyz/teamgravity/cleantheming/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/cleantheming/MainActivity.kt
@@ -1,13 +1,20 @@
 package xyz.teamgravity.cleantheming
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import xyz.teamgravity.cleantheming.ui.theme.CleanThemingTheme
+import xyz.teamgravity.cleantheming.ui.theme.spacing
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +22,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             CleanThemingTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Button(
+                            modifier = Modifier
+                                .padding(horizontal = MaterialTheme.spacing.medium)
+                                .fillMaxWidth(),
+                            onClick = {
+                                Toast.makeText(this@MainActivity, "Button clicked", Toast.LENGTH_SHORT).show()
+                            }
+                        ) {
+                            Text(text = "Simple button")
+                        }
+                    }
                 }
             }
         }
