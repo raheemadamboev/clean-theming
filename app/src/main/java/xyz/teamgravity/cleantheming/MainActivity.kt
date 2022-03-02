f7297d848b9dc7f0c8a512dcc480e953510b70da
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-02 11:06:56 +0500

    depeendencies setup

--- app/src/main/java/xyz/teamgravity/cleantheming/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/cleantheming/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.cleantheming.ui.theme.CleanThemingTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +14,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CleanThemingTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CleanThemingTheme {
-        Greeting("Android")
     }
 }
